713a81406ff23d4b97e6f0899e911152e7e99a78
Ho-Wan <ho.wan2345@gmail.com>
2021-02-28 11:51:21 +0000

    ex5

--- 5-session-cleaner/main.go
+++ 5-session-cleaner/main.go
@@ -20,17 +20,25 @@ package main
 import (
 	"errors"
 	"log"
+	"sync"
+	"time"
+)
+
+const (
+	timeLimit = 5
 )
 
 // SessionManager keeps track of all sessions from creation, updating
 // to destroying.
 type SessionManager struct {
 	sessions map[string]Session
+	mu       sync.Mutex
 }
 
 // Session stores the session's data
 type Session struct {
-	Data map[string]interface{}
+	Data  map[string]interface{}
+	Timer *time.Timer
 }
 
 // NewSessionManager creates a new sessionManager
@@ -49,11 +57,33 @@ func (m *SessionManager) CreateSession() (string, error) {
 		return "", err
 	}
 
-	m.sessions[sessionID] = Session{
-		Data: make(map[string]interface{}),
+	timer := time.NewTimer(timeLimit * time.Second)
+
+	s := Session{
+		Data:  make(map[string]interface{}),
+		Timer: timer,
 	}
 
+	m.mu.Lock()
+	m.sessions[sessionID] = s
+	m.mu.Unlock()
+
+	log.Println("created session: ", sessionID)
+
+	go m.CleanSession(&s, sessionID)
+
 	return sessionID, nil
+}
+
+// CleanSession - cleans session if time limit reached
+func (m *SessionManager) CleanSession(s *Session, sID string) {
+	<-s.Timer.C
+
+	m.mu.Lock()
+	delete(m.sessions, sID)
+	m.mu.Unlock()
+
+	log.Println("deleted session: ", sID)
 }
 
 // ErrSessionNotFound returned when sessionID not listed in
@@ -63,23 +93,36 @@ var ErrSessionNotFound = errors.New("SessionID does not exists")
 // GetSessionData returns data related to session if sessionID is
 // found, errors otherwise
 func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{}, error) {
+
+	m.mu.Lock()
 	session, ok := m.sessions[sessionID]
+	m.mu.Unlock()
 	if !ok {
 		return nil, ErrSessionNotFound
 	}
+
 	return session.Data, nil
 }
 
 // UpdateSessionData overwrites the old session data with the new one
 func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]interface{}) error {
-	_, ok := m.sessions[sessionID]
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	s, ok := m.sessions[sessionID]
 	if !ok {
 		return ErrSessionNotFound
 	}
 
+	// reset timer here
+	timer := s.Timer
+	timer.Stop()
+	timer.Reset(timeLimit * time.Second)
+
 	// Hint: you should renew expiry of the session here
 	m.sessions[sessionID] = Session{
-		Data: data,
+		Data:  data,
+		Timer: timer,
 	}
 
 	return nil
